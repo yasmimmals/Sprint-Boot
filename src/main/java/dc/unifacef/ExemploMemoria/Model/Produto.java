c7578d54de71b2fb46c1fdb2fac6d69c1d5f721a
Yasmim <yasmimalsferreira@gmail.com>
2026-05-04 22:07:07 -0300

    Hello World 2

---
+++ src/main/java/dc/unifacef/ExemploMemoria/Model/Produto.java
@@ -0,0 +1,47 @@
+package dc.unifacef.ExemploMemoria.Model;
+
+public class Produto {
+    private Long id;
+    private String nome;
+    private Double preco;
+    private Integer estoque;
+
+    public Produto(Long id, String nome, Double preco, Integer estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
